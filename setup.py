90427b10aed93488407d1e9e60f286b8f41f7ab3
tes3awy <11368432+Tes3awy@users.noreply.github.com>
2023-02-20 23:16:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from setuptools import find_packages, setup
 
 setup(
     name="PSI",
+    author="Osama Abbas",
+    author_email="oabbas2512@gmail.com",
+    maintainer="Osama Abbas",
+    maintainer_email="oabbas2512@gmail.com",
+    url="https://github.com/Tes3awy/PSIRT",
+    keywords=["Cisco", "PSIRT"], 
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
+    license="LICENSE",
     install_requires=[
         "wheel",
         "Flask",

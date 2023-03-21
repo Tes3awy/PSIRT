aa515914aa45c769f57fda5dfd48393269ae8045
tes3awy <11368432+Tes3awy@users.noreply.github.com>
2023-03-21 23:26:35 +0200

    Refactor error handling

--- psiapp/errors/handlers.py
+++ psiapp/errors/handlers.py
@@ -1,34 +1,22 @@
 from flask import Blueprint, render_template
 from flask_wtf.csrf import CSRFError
+from werkzeug.exceptions import (
+    BadRequest,
+    Forbidden,
+    InternalServerError,
+    NotFound,
+    TooManyRequests,
+    Unauthorized,
+)
 
 errors_bp = Blueprint("errors", __name__)
 
-
-# Error 400 Page (Bad Request)
 @errors_bp.app_errorhandler(CSRFError)
-def error_400(e):
-    return render_template("errors/400.html", title=str(e.code), e=e), 400
-
-
-# Error 403 Page (Forbidden)
-@errors_bp.app_errorhandler(403)
-def error_403(e):
-    return render_template("errors/403.html", title=str(e.code), e=e), 403
-
-
-# Error 404 Page (Page Not Found)
-@errors_bp.app_errorhandler(404)
-def error_404(e):
-    return render_template("errors/404.html", title=str(e.code), e=e), 404
-
-
-# Error 429 Page (Rate Limit Exceeded)
-@errors_bp.app_errorhandler(429)
-def error_429(e):
-    return render_template("errors/429.html", title=str(e.code), e=e), 429
-
-
-# Error 500 Page (Internal Server Error)
-@errors_bp.app_errorhandler(500)
-def error_500(e):
-    return render_template("errors/500.html", title=str(e.code), e=e), 500
+@errors_bp.app_errorhandler(BadRequest)
+@errors_bp.app_errorhandler(Unauthorized)
+@errors_bp.app_errorhandler(Forbidden)
+@errors_bp.app_errorhandler(NotFound)
+@errors_bp.app_errorhandler(TooManyRequests)
+@errors_bp.app_errorhandler(InternalServerError)
+def handle_http_error(error):
+    return render_template("error.html", title=error.name, error=error), error.code

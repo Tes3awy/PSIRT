0b5a7815d7423eccbc32dffab40614452c7d1d63
tes3awy <11368432+Tes3awy@users.noreply.github.com>
2023-04-08 23:31:25 +0200

    Add date range search

---
+++ psiapp/dates/forms.py
@@ -0,0 +1,39 @@
+from datetime import date, timedelta
+
+from flask_wtf import FlaskForm
+from wtforms.fields import DateField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class DateRangeSearchForm(FlaskForm):
+    date_sort = SelectField(
+        label="Published by",
+        coerce=str,
+        validate_choice=True,
+        choices=[
+            ("firstpublished", "First Published"),
+            ("lastpublished", "Last Published"),
+        ],
+        validators=[DataRequired()],
+        default=["firstpublished"],
+        render_kw={"placeholder": "First Published", "class_": "focus"},
+    )
+    start_date = DateField(
+        label="Start Date",
+        validators=[DataRequired()],
+        render_kw={"placeholder": "Start Date", "min": date(1999, 9, 1)},
+        format="%Y-%m-%d",
+    )
+    end_date = DateField(
+        label="End Date",
+        validators=[DataRequired()],
+        render_kw={"placeholder": "End Date"},
+        format="%Y-%m-%d",
+    )
+
+    def validate_end_date(self, end_date):
+        if self.start_date.data > end_date.data:
+            raise ValidationError("End Date must be greater than Start Date")
+
+        if end_date.data > date.today():
+            raise ValidationError("Cannot get advisories of future dates")
---
+++ psiapp/dates/views.py
@@ -0,0 +1,100 @@
+from datetime import date, timedelta
+
+import requests
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+
+from psiapp import limiter
+from psiapp.dates.forms import DateRangeSearchForm
+from psiapp.utils import fetch_data, paginate
+
+dates_bp = Blueprint("dates", __name__, url_prefix="/all")
+
+
+# By Date Range Search Form Page
+@dates_bp.route("/", methods=["GET", "POST"])
+@limiter.exempt
+def dates_range(title="Search by date range"):
+    form = DateRangeSearchForm()
+    if form.validate_on_submit():
+        date_sort = form.date_sort.data
+        startDate = form.start_date.data
+        endDate = form.end_date.data
+        return redirect(
+            url_for(
+                "dates.results",
+                date_sort=date_sort,
+                startDate=startDate,
+                endDate=endDate,
+            )
+        )
+    return render_template(
+        "dates/form.html", title=title, form=form, date=date, timedelta=timedelta
+    )
+
+
+# By Date Range Search Results Page
+@dates_bp.route("/<string:date_sort>", methods=["GET"])
+def results(date_sort: str):
+    pageIndex = request.args.get("pageIndex", 1, int)
+    pageSize = request.args.get("pageSize", 10, int)
+    startDate = request.args.get("startDate", None)
+    endDate = request.args.get("endDate", None)
+    print(startDate, endDate)
+    title = f"Search results from {startDate} to {endDate} (by {date_sort})"
+    try:
+        res = fetch_data(
+            uri=f"all/{date_sort}?pageIndex={pageIndex}&pageSize={pageSize}&startDate={startDate}&endDate={endDate}&productNames=false",
+            access_token=session.get("access_token"),
+        )
+    except requests.exceptions.ConnectionError as e:
+        flash("Connection Error! Failed to establish a connection", category="danger")
+        return redirect(url_for(".dates_range"))
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 403:
+            flash(
+                "Session has expired! You are redirected here to refresh your session",
+                category="info",
+            )
+            return redirect(url_for("main.index"))
+        if e.response.status_code in [404, 406]:
+            flash(
+                f"{e.response.json().get('errorCode')} - {e.response.json().get('errorMessage')}",
+                category="warning" if e.response.status_code == 404 else "danger",
+            )
+            return redirect(url_for(".dates_range"))
+        if e.response.status_code == 503:
+            flash(
+                "Service is currently unavailable from Cisco! Please try again later",
+                category="danger",
+            )
+            return redirect(url_for(".dates_range"))
+        flash(str(e), category="danger")
+        return redirect(url_for(".dates_range"))
+    else:
+        advisories = res.json().get("advisories")
+        paging = paginate(
+            paging=res.json().get("paging"),
+            pageIndex=pageIndex,
+            pageSize=pageSize,
+        )
+        total_count = paging.get("total_count")
+        tnp = paging.get("tnp")  # total number of pages
+        start = paging.get("start")
+        end = paging.get("end")
+        pagination = res.json().get("paging")
+        flash(
+            f"{f'Page {pageIndex} of {tnp} - ' if pageIndex != tnp else ''}{title}",
+            "success",
+        )
+        return render_template(
+            "dates/results.html",
+            title=title,
+            advisories=advisories,
+            pagination=pagination,
+            total_count=total_count,
+            start_date=startDate,
+            end_date=endDate,
+            tnp=tnp,
+            start=start + 1,
+            end=end,
+        )

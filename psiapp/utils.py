9876142e49a47a0a528e9560e9e7c06db78d7f19
Osama Abbas <11368432+Tes3awy@users.noreply.github.com>
2023-11-04 16:09:24 +0200

    Add BearerAuth class

--- psiapp/utils.py
+++ psiapp/utils.py
@@ -4,14 +4,20 @@ import requests
 from flask import current_app
 
 
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["Authorization"] = f"Bearer {self.token}"
+        return r
+
+
 def fetch_data(uri: str, access_token: str) -> requests.Response:
     r = requests.get(
         url=f"{current_app.config.get('BASE_URL')}/{uri}",
-        headers={
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": f"Bearer {access_token}",
-        },
+        headers={"Content-Type": "application/json", "Accept": "application/json"},
+        auth=BearerAuth(access_token),
         verify=False,
     )
     r.raise_for_status()
